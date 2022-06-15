6fa2e1d5b070cbd926e85fe9f629736abe9d79b1
Victoria Cheng <victoria.cheng15@gmail.com>
2022-06-15 17:09:35 -0400

    add insert tea operation

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
-const express = require('express')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const { MongoClient } = require('mongodb');
 
-app.use(express.json())
+const app = express();
 
-app.get('/', (req,res) => {
-  res.sendFile(__dirname + '/index.html')
-})
+const PORT = process.env.PORT || 8000;
+const url = process.env.DB_URL;
+let db;
+const dbName = 'bubble-tea-api';
 
+MongoClient.connect(url, { useUnifiedTopology: true })
+  .then((client) => {
+    console.log('connect to database');
+    db = client.db(dbName);
+  })
+  .catch((err) => console.log(err));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-const PORT = 3000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.get('/', (request, response) => {
+  response.sendFile(`${__dirname}/index.html`);
+});
+
+app.post('/teas', (request, response) => {
+  db.collection('teas').insertOne(request.body)
+    .then((result) => {
+      response.redirect('/');
+    });
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
