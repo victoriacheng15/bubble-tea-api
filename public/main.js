7ac592e8fc621bf809b4e985b5e83221eca13829
Victoria Cheng <victoria.cheng15@gmail.com>
2022-06-20 17:41:43 -0400

    add drag and drop logic for main js

--- public/main.js
+++ public/main.js
@@ -1,36 +1,37 @@
 let dragItem = null;
-function dragStart() {
-  dragItem = this;
-  // setTimeout(() => (this.className = 'invisible'), 0);
-  console.log('dragstart');
+
+function handleDragStart() {
+  dragItem = this.textContent;
+  this.style.opacity = '0.4';
+  setTimeout(() => (this.className = 'invisible'), 0);
 }
 
-function dragEnd() {
-  // this.className = 'item';
-  // dragItem = null;
-  console.log('dragend');
+function handleDragEnd() {
+  this.className = 'item';
+  dragItem = null;
+  this.style.opacity = '1';
 }
 
-function dragOver(e) {
-  // e.preventDefault();
-  console.log('dragover');
-  console.log(this);
+function handleDragOver(e) {
+  e.preventDefault();
+  this.style.backgroundColor = 'lightcyan';
 }
 
-function dragDrop() {
-  // this.append(dragItem);
-  console.log('drop');
+function handleDragDrop() {
+  this.style.backgroundColor = 'lightblue';
+  const input = document.createElement('input');
+  input.classList.add('input');
+  input.value = dragItem;
+  this.appendChild(input);
 }
 
-(function () {
-  const items = document.querySelectorAll('.item');
-  items.forEach((item) => {
-    item.addEventListener('dragstart', dragStart);
-    item.addEventListener('dragend', dragEnd);
-  });
+const items = document.querySelectorAll('.item');
+items.forEach((item) => {
+  item.addEventListener('dragstart', handleDragStart);
+  item.addEventListener('dragend', handleDragEnd);
+});
 
-  // order box
-  const order = document.querySelector('.order');
-  order.addEventListener('dragover', dragOver);
-  order.addEventListener('dragdrop', dragDrop);
-}());
+// order box
+const orderBox = document.querySelector('.order-box');
+orderBox.addEventListener('dragover', handleDragOver);
+orderBox.addEventListener('drop', handleDragDrop);
