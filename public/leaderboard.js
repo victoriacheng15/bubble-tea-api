3b5fde91b05ff69050e5b232f1ca16e7e708a146
Victoria Cheng <victoria.cheng15@gmail.com>
2022-06-28 14:45:39 -0400

    use regex to extract number for calculating percentage

--- public/leaderboard.js
+++ public/leaderboard.js
@@ -2,8 +2,12 @@
   const lists = [...document.querySelectorAll('.leaderboard-list')];
   const leadingBackground = [...document.querySelectorAll('.leading-background')];
   const values = [];
-  lists.forEach((list) => values.push(list.innerText.slice(-1)));
+  lists.forEach((list) => {
+    const regex = /[a-z\W]/gi;
+    const num = list.innerText.replace(regex, '');
+    values.push(num);
+  });
   const totalSum = values.reduce((acc, curr) => acc + +curr, 0);
-  const getPercentage = values.map(((num) => (num / totalSum) * 100));
+  const getPercentage = [...values].map(((num) => ((num / totalSum) * 100).toFixed(2)));
   leadingBackground.forEach((elem, index) => elem.style.width = `${getPercentage[index]}%`);
 }());
