77064d97ab42baa417e0e4d1e0bbf9a3d8dd456a
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2022-10-31 13:48:19 -0600

    Update packages (#34)
    * chore: remove unused files
    
    * builds: update all packages to latest
    
    * builds: add eslint and prettier, and add lint and format for scripts
    
    * styles: run formatter

--- public/main.js
+++ public/main.js
@@ -1,19 +1,19 @@
-const teasCol = document.querySelector('.teas-list');
-const toppingsCol = document.querySelector('.toppings-list');
-const items = document.querySelectorAll('.item');
-const orderBox = document.querySelector('.order-box');
-const submitBtn = document.querySelector('.submit-btn');
-const clearBtn = document.querySelector('.clear-btn');
+const teasCol = document.querySelector(".teas-list");
+const toppingsCol = document.querySelector(".toppings-list");
+const items = document.querySelectorAll(".item");
+const orderBox = document.querySelector(".order-box");
+const submitBtn = document.querySelector(".submit-btn");
+const clearBtn = document.querySelector(".clear-btn");
 let dragItem = null;
 
 function handleDragStart() {
   dragItem = this;
-  this.style.opacity = '0.4';
+  this.style.opacity = "0.4";
 }
 
 function handleDragEnd() {
   dragItem = null;
-  this.style.opacity = '1';
+  this.style.opacity = "1";
 }
 
 function handleDragOver(e) {
@@ -21,45 +21,47 @@ function handleDragOver(e) {
 }
 
 function noPointerClass(arr, action) {
-  return action === 'add'
-    ? arr.forEach(((elem) => elem.classList.add('no-pointer')))
-    : arr.forEach(((elem) => elem.classList.remove('no-pointer')));
+  return action === "add"
+    ? arr.forEach((elem) => elem.classList.add("no-pointer"))
+    : arr.forEach((elem) => elem.classList.remove("no-pointer"));
 }
 
 function disabledButton(boolean) {
-  boolean ? submitBtn.disabled = true : submitBtn.disabled = false;
+  boolean ? (submitBtn.disabled = true) : (submitBtn.disabled = false);
 }
 
 function disableDrags() {
-  const inputElems = document.querySelectorAll('input');
+  const inputElems = document.querySelectorAll("input");
   if (inputElems.length === 2) {
-    noPointerClass([teasCol, toppingsCol], 'add');
+    noPointerClass([teasCol, toppingsCol], "add");
   }
 }
 
 function enbleSubmit() {
-  const inputElems = document.querySelectorAll('input');
-  const checkForTea = [...inputElems].some((input) => input.name === 'tea');
-  const checkForTopping = [...inputElems].some((input) => input.name === 'topping');
+  const inputElems = document.querySelectorAll("input");
+  const checkForTea = [...inputElems].some((input) => input.name === "tea");
+  const checkForTopping = [...inputElems].some(
+    (input) => input.name === "topping"
+  );
   if (inputElems.length === 2 && checkForTea && checkForTopping) {
     disabledButton(false);
   }
 }
 
 function createInputElemets(text) {
-  const input = document.createElement('input');
-  const typeForInputs = (word) => word === 'Tea';
+  const input = document.createElement("input");
+  const typeForInputs = (word) => word === "Tea";
   const textValue = text;
-  input.classList.add('input');
-  input.type = 'text';
+  input.classList.add("input");
+  input.type = "text";
   input.value = textValue;
-  input.name = textValue.split(' ').some(typeForInputs) ? 'tea' : 'topping';
+  input.name = textValue.split(" ").some(typeForInputs) ? "tea" : "topping";
   input.readOnly = true;
   orderBox.appendChild(input);
 }
 
 function handleDragDrop() {
-  this.style.backgroundColor = '#ece0d1';
+  this.style.backgroundColor = "#ece0d1";
   createInputElemets(dragItem.innerText);
   disableDrags();
   enbleSubmit();
@@ -74,45 +76,45 @@ function clickAddChoices(e) {
 
 function reset(e) {
   e.preventDefault();
-  const inputElems = [...document.querySelectorAll('input')];
+  const inputElems = [...document.querySelectorAll("input")];
   inputElems.forEach((element) => element.remove());
   noPointerClass([teasCol, toppingsCol]);
   disabledButton(true);
 }
 
 function clickAddEvent() {
-  items.forEach((item) => item.addEventListener('click', clickAddChoices));
+  items.forEach((item) => item.addEventListener("click", clickAddChoices));
 }
 
 function removeClickAddEvent() {
-  items.forEach((item) => item.removeEventListener('click', clickAddChoices));
+  items.forEach((item) => item.removeEventListener("click", clickAddChoices));
 }
 
 function dragAndDropEvent() {
   items.forEach((item) => {
-    item.addEventListener('dragstart', handleDragStart);
-    item.addEventListener('dragend', handleDragEnd);
+    item.addEventListener("dragstart", handleDragStart);
+    item.addEventListener("dragend", handleDragEnd);
   });
 
-  orderBox.addEventListener('dragover', handleDragOver);
-  orderBox.addEventListener('drop', handleDragDrop);
+  orderBox.addEventListener("dragover", handleDragOver);
+  orderBox.addEventListener("drop", handleDragDrop);
 }
 
 function removeDnDEvent() {
   items.forEach((item) => {
-    item.removeEventListener('dragstart', handleDragStart);
-    item.removeEventListener('dragend', handleDragEnd);
+    item.removeEventListener("dragstart", handleDragStart);
+    item.removeEventListener("dragend", handleDragEnd);
   });
 
-  orderBox.removeEventListener('dragover', handleDragOver);
-  orderBox.removeEventListener('drop', handleDragDrop);
+  orderBox.removeEventListener("dragover", handleDragOver);
+  orderBox.removeEventListener("drop", handleDragDrop);
 }
 
 function displayWidthAndAction(width, defaultWidth) {
-  const showWidth = document.querySelector('[data-width]');
-  const showAction = document.querySelector('[data-action]');
+  const showWidth = document.querySelector("[data-width]");
+  const showAction = document.querySelector("[data-action]");
   showWidth.textContent = `${width} px`;
-  showAction.textContent = `${width > defaultWidth ? 'drag' : 'click'}`;
+  showAction.textContent = `${width > defaultWidth ? "drag" : "click"}`;
 }
 
 function checkWidth() {
@@ -134,11 +136,11 @@ function checkWidth() {
 
 let timeout = false;
 
-window.addEventListener('load', checkWidth);
-window.addEventListener('resize', () => {
+window.addEventListener("load", checkWidth);
+window.addEventListener("resize", () => {
   clearTimeout(timeout);
   timeout = setTimeout(checkWidth, 250);
 });
 
 // reset
-clearBtn.addEventListener('click', reset);
+clearBtn.addEventListener("click", reset);
