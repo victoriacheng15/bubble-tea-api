f21fbbb88b33b10f81394c882488210f05cf95a6
Victoria Cheng <victoria.cheng15@gmail.com>
2022-12-27 14:19:09 -0700

    chore: change to async and await

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-require("dotenv").config();
 const express = require("express");
 const { MongoClient } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 
@@ -8,45 +8,48 @@ const PORT = process.env.PORT || 8000;
 const url = process.env.MONGODB_URI;
 
 let db;
+let client;
 const dbName = "bubble-tea-api";
 
-MongoClient.connect(url, { useUnifiedTopology: true })
-  .then((client) => {
-    console.log("connect to database");
+const connectDB = async () => {
+  try {
+    client = await MongoClient.connect(url, { useUnifiedTopology: true });
     db = client.db(dbName);
-  })
-  .catch((err) => console.log("mongodb is not connected", err));
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-app.set("vew engine", "ejs");
+connectDB();
+
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/", (request, response) => {
-  const teasCollection = db.collection("teas");
-  teasCollection
-    .find()
-    .toArray()
-    .then((results) => {
-      response.render("index.ejs", { teas: results });
-    })
-    .catch((err) => console.log(err));
+app.get("/", async (req, res) => {
+  try {
+    const teasCollection = await db.collection("teas");
+    const result = await teasCollection.find().toArray();
+    res.render("index", { teas: result });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-app.get("/history", (request, response) => {
-  response.render("history.ejs");
+app.get("/history", (req, res) => {
+  res.render("history");
 });
 
-app.get("/leaderboard", (request, response) => {
-  // response.redirect('/history.ejs');
-  const orderCollection = db.collection("orders");
-  orderCollection
-    .find()
-    .sort({ count: -1 })
-    .toArray()
-    .then((results) => {
-      response.render("leaderboard.ejs", { orders: results });
-    });
+app.get("/leaderboard", async (req, res) => {
+  try {
+    const orderCollection = await db.collection("orders");
+    const results = await orderCollection.find().sort({ count: -1 }).toArray();
+    res.render("leaderboard", { orders: results });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 // this was for sending data of teas and columns to the database's collection named teas
@@ -57,29 +60,30 @@ app.get("/leaderboard", (request, response) => {
 //     });
 // });
 
-app.post("/order", (request, response) => {
-  const { tea, topping } = request.body;
-  const orderCollection = db.collection("orders");
-  orderCollection
-    .find({ tea, topping })
-    .toArray()
-    .then((result) => {
-      if (result) {
-        orderCollection.updateOne(
-          { tea, topping },
-          { $inc: { count: 1 } },
-          { upsert: true }
-        );
-      } else if (!result) {
-        orderCollection.insertOne({
-          tea,
-          topping,
-          count: 1,
-        });
-      }
-      response.redirect("/");
-    })
-    .catch((err) => console.log(err));
+app.post("/order", async (req, res) => {
+  const { tea, topping } = req.body;
+  try {
+    const orderCollection = await db.collection("orders");
+    const results = await orderCollection.find({ tea, topping }).toArray();
+
+    if (results) {
+      orderCollection.updateOne(
+        { tea, topping },
+        { $inc: { count: 1 } },
+        { upsert: true }
+      );
+    } else {
+      orderCollection.insertOne({
+        tea,
+        topping,
+        count: 1,
+      });
+    }
+
+    res.redirect("/leaderboard");
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
