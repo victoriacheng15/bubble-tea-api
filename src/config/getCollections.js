2422a477a04933dae13e193e290d2ebf49b1f0bf
Victoria Cheng <victoria.cheng15@Gmail.com>
2023-12-13 06:47:43 -0700

    style: the files have been formatted

--- src/config/getCollections.js
+++ src/config/getCollections.js
@@ -1,27 +1,27 @@
-async function getTeas (db) {
-  const teasCollection = db.collection("teas");
-  return teasCollection.find().toArray();
-};
+async function getTeas(db) {
+	const teasCollection = db.collection("teas");
+	return teasCollection.find().toArray();
+}
 
-async function getOrders (db) {
-  const orderCollection = db.collection("orders");
-  return orderCollection.find().sort({ count: -1 }).toArray();
-};
+async function getOrders(db) {
+	const orderCollection = db.collection("orders");
+	return orderCollection.find().sort({ count: -1 }).toArray();
+}
 
-async function updateOrderCount (db, tea, topping) {
-  const orderCollection = db.collection("orders");
+async function updateOrderCount(db, tea, topping) {
+	const orderCollection = db.collection("orders");
 
-  const order = await orderCollection.find({ tea, topping }).toArray();
+	const order = await orderCollection.find({ tea, topping }).toArray();
 
-  if (order.length > 0) {
-    orderCollection.updateOne({ tea, topping }, { $inc: { count: 1 } });
-  } else {
-    orderCollection.insertOne({
-      tea,
-      topping,
-      count: 1,
-    });
-  }
-};
+	if (order.length > 0) {
+		orderCollection.updateOne({ tea, topping }, { $inc: { count: 1 } });
+	} else {
+		orderCollection.insertOne({
+			tea,
+			topping,
+			count: 1,
+		});
+	}
+}
 
 module.exports = { getTeas, getOrders, updateOrderCount };
