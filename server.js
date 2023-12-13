54b367e3fc977e358dd655cd2fe88045ca8f4c4a
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-12-13 06:48:03 -0700

    add biomejs (#50)
    * feat: remove prettier and eslint and add biomejs as the formatter
    
    * style: the files have been formatted

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const connectDb = require("./src/config/connectDb");
 const {
-  getTeas,
-  getOrders,
-  updateOrderCount,
+	getTeas,
+	getOrders,
+	updateOrderCount,
 } = require("./src/config/getCollections");
 require("dotenv").config();
 
@@ -18,46 +18,46 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  try {
-    const db = await connectDb(url);
-    const teas = await getTeas(db);
-    res.render("index", { teas });
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		const db = await connectDb(url);
+		const teas = await getTeas(db);
+		res.render("index", { teas });
+	} catch (error) {
+		console.log(error);
+	}
 });
 
 app.get("/history", (req, res) => {
-  res.render("history");
+	res.render("history");
 });
 
 app.get("/leaderboard", async (req, res) => {
-  try {
-    const db = await connectDb(url);
-    const orders = await getOrders(db);
-    res.render("leaderboard", { orders });
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		const db = await connectDb(url);
+		const orders = await getOrders(db);
+		res.render("leaderboard", { orders });
+	} catch (error) {
+		console.log(error);
+	}
 });
 
 app.post("/order", async (req, res) => {
-  const { tea, topping } = req.body;
+	const { tea, topping } = req.body;
 
-  try {
-    const db = await connectDb(url);
-    await updateOrderCount(db, tea, topping);
-    res.redirect("/leaderboard");
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		const db = await connectDb(url);
+		await updateOrderCount(db, tea, topping);
+		res.redirect("/leaderboard");
+	} catch (error) {
+		console.log(error);
+	}
 });
 
 (async () => {
-  try {
-    await connectDb(url);
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  } catch (err) {
-    console.error(err);
-  }
+	try {
+		await connectDb(url);
+		app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+	} catch (err) {
+		console.error(err);
+	}
 })();
