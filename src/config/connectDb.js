57158eb738b755d558a40b2d7494d2e3ce93145d
Victoria Cheng <victoria.cheng15@Gmail.com>
2023-12-13 06:34:24 -0700

    refactor: extract connectDb function out of the main

---
+++ src/config/connectDb.js
@@ -0,0 +1,9 @@
+const { MongoClient } = require("mongodb");
+
+const connectDb = async (url) => {
+  const client = new MongoClient(url);
+  await client.connect();
+  return client.db("bubble-tea-api");
+};
+
+module.exports = connectDb;
