958b5f6abe9414514472b0981bf971af1936dd47
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-12-14 09:30:01 -0700

    refactor routes controllers (#51)
    * chore: move getCollections to util folder
    
    * refactor: add controllers for each route
    
    * refactor: add router for each operation
    
    * refactor: add router for app use
    
    * style: the files have been formatted

---
+++ src/controllers/allControllers.js
@@ -0,0 +1,52 @@
+const connectDb = require("../config/connectDb");
+const {
+	getTeas,
+	getOrders,
+	updateOrderCount,
+} = require("../utils/getCollections");
+
+require("dotenv").config();
+const url = process.env.MONGODB_URI;
+
+async function homePage(req, res) {
+	try {
+		const db = await connectDb(url);
+		const teas = await getTeas(db);
+		res.render("index", { teas });
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+async function historyPage(req, res) {
+	res.render("history");
+}
+
+async function leaderboardPage(req, res) {
+	try {
+		const db = await connectDb(url);
+		const orders = await getOrders(db);
+		res.render("leaderboard", { orders });
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+async function updateOrder(req, res) {
+	const { tea, topping } = req.body;
+
+	try {
+		const db = await connectDb(url);
+		await updateOrderCount(db, tea, topping);
+		res.redirect("/leaderboard");
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+module.exports = {
+	homePage,
+	historyPage,
+	leaderboardPage,
+	updateOrder,
+};
---
+++ src/routes/allRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const {
+	homePage,
+	historyPage,
+	leaderboardPage,
+	updateOrder,
+} = require("../controllers/allControllers");
+
+router.get("/", homePage);
+router.get("/history", historyPage);
+router.get("/leaderboard", leaderboardPage);
+router.post("/order", updateOrder);
+
+module.exports = router;
